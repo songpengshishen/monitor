ea431a4c13e7768833eb40d46a1d1de93871a9b1
wangsongpeng <wangsongpeng@jd.com>
2018-01-25 14:05:59 +0800

    JDBC接口开发

--- monitor-resource/src/main/java/com/jd/nb/monitor/resource/db/jdbc/ds/AbstractDataSource.java
+++ monitor-resource/src/main/java/com/jd/nb/monitor/resource/db/jdbc/ds/AbstractDataSource.java
@@ -1,6 +1,9 @@
 package com.jd.nb.monitor.resource.db.jdbc.ds;
 import com.jd.nb.monitor.resource.db.jdbc.proxy.ProxyDataSource;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * 抽象的DataSource,负责高层各种DataSource的复用
  * @author <a href=mailto:wangsongpeng@jd.com>王宋鹏</a>
@@ -8,7 +11,10 @@ import com.jd.nb.monitor.resource.db.jdbc.proxy.ProxyDataSource;
  */
 public abstract class AbstractDataSource extends ProxyDataSource {
 
-
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return getConnection();
+    }
 
 
 }
--- monitor-resource/src/main/java/com/jd/nb/monitor/resource/db/jdbc/proxy/ProxyDataSource.java
+++ monitor-resource/src/main/java/com/jd/nb/monitor/resource/db/jdbc/proxy/ProxyDataSource.java
@@ -1,6 +1,7 @@
 package com.jd.nb.monitor.resource.db.jdbc.proxy;
 import javax.sql.DataSource;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 import java.util.logging.Logger;
